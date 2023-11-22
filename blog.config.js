a5325930089d8c45f3baa25f0f73cfb042991aea
DragonYLord <148200212+DragonYLord@users.noreply.github.com>
2023-11-22 11:19:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,6 +25,8 @@ const BLOG = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_ITCH: process.env.NEXT_PUBLIC_CONTACT_ITCH || 'https://longxiaojie.itch.io/', // itch主页
+  CONTACT_DOUBAN: process.env.NEXT_PUBLIC_CONTACT_DOUBAN || 'https://www.douban.com/people/137359056/', // 豆瓣主页
+  CONTACT_STEAM: process.env.NEXT_PUBLIC_CONTACT_STEAM || 'https://steamcommunity.com/profiles/76561198378988197/', // steam主页
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
